0cb0545b459cb4d5abc9b5aa0f1909ae8409e9aa
ChromosomeYUXA <124550832+ChromosomeYUXA@users.noreply.github.com>
2024-03-17 14:51:48 +0800

    initial Commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by YUXA on 2024/3/17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
