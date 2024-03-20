c4ed9b49a78af3a9e7e65b70a816be5012db80c3
ChromosomeYUXA <1928755665@qq.com>
2024-03-20 12:04:00 +0800

    arrange the layout the cards' layout are more beautiful, and you can scroll up and down. learned to use Group to deal with more views in one time.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -13,33 +13,54 @@ struct ContentView: View {
     @State var cardCount: Int = 4
     
     var body: some View {
+        
         VStack{
-            HStack{
-                ForEach(0..<cardCount, id: \.self) {index in
-                    CardView(content: emojis[index])
-                }
+            ScrollView{
+                Cards
             }
-            .foregroundColor(.orange)
-            
-            HStack{
-                Button(action: {
-                    cardCount -= 1
-                }, label: {
-                    Image(systemName: "rectangle.stack.fill.badge.plus")
-                })
-                Spacer()
-                Button(action: {
-                    cardCount += 1
-                }, label: {
-                    Image(systemName: "rectangle.stack.fill.badge.minus")
-                })
-            }
-            .imageScale(.large)
-            .font(.largeTitle)
-            
+            Spacer()
+            cardCountAdjusters
         }
         .padding()
+        
     }
+    
+    var Cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount, id: \.self) {index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+                cardCount += offset
+        }, label: {
+                Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")    }
+    
 }
 
 struct CardView: View {
@@ -49,19 +70,19 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let  base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2 )
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill()
-                
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture {
             isFaceUp.toggle()
         }
     }
+    
 }
 
 
